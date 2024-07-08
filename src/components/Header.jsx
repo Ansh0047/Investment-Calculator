a3004a1efbc408cb079cbb1e3416d150fa1dba49
Ansh <anshkant090@gmail.com>
2024-07-08 22:31:55 +0530

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import HeaderImg from '../assets/investment-calculator-logo.png';
+
+export default function Header(){
+    return <header>
+        <img src={HeaderImg} alt='logo of money bank'></img>
+        <h1>Investment Calculator</h1>
+        
+    </header>
+}
