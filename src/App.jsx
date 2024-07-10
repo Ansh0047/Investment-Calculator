893f0d5e93c0ee3aa170f983e5bf13feb2c9dfa4
Ansh <anshkant090@gmail.com>
2024-07-10 22:30:38 +0530

    Lift the state to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 
 function App() {
+  // Two way binding - to ensure that the state and the UI are always sync
+  // this useState for managing and storing the input provided by the user
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  // State Change fucntion : to update the data on the ui when user enters the input
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput, // this is syntax is used to copy the data stored in the prevUserInput
+        [inputIdentifier]: newValue, // and this JS syntax is used to set the object's property value
+      };
+    });
+  }
+
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onSelect={handleChange}/>
+      <Results input={userInput}/>
     </>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,46 +1,57 @@
-import { useState } from "react"
+import { useState } from "react";
 
+export default function UserInput({userInput, onSelect}) {
 
-export default function UserInput(){
-    // Two way binding
-    // this useState for managing and storing the input provided by the user
-    const [userInput,setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
-    // to update the data on the ui when user enters the input
-    function handleChange(inputIdentifier, newValue){
-        setUserInput((prevUserInput) => {
-            return {
-                ...prevUserInput,            // this is syntax is used to copy the data stored in the prevUserInput
-                [inputIdentifier]: newValue    // and this JS syntax is used to set the object's property value 
+  return (
+    <form id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+                onSelect("initialInvestment", event.target.value)
             }
-        });
-    }
-
-    return <form id="user-input">
-        <div className="input-group">
-            <p>
-                <label>Initial Investment</label>
-                <input type="number" required onChange={(event) => handleChange('initialInvestment',event.target.value)}/>
-            </p>
-            <p>
-                <label>Annual Investment</label>
-                <input type="number" required />
-            </p>
-        </div>
-        <div className="input-group">
-            <p>
-                <label>Expected Return</label>
-                <input type="number" required />
-            </p>
-            <p>
-                <label>Duration</label>
-                <input type="number" required />
-            </p>
-        </div>
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+                onSelect("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+                onSelect("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) =>
+                onSelect("duration", event.target.value)
+            }
+          />
+        </p>
+      </div>
     </form>
+  );
 }
