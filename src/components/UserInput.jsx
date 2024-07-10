06ec568c93c3d7c2e442be3af447b675c1a3489d
Ansh <anshkant090@gmail.com>
2024-07-10 20:29:20 +0530

    Two way binding to set User Input and State change UI

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 
 export default function UserInput(){
+    // Two way binding
+    // this useState for managing and storing the input provided by the user
     const [userInput,setUserInput] = useState({
         initialInvestment: 10000,
         annualInvestment: 1200,
@@ -9,15 +11,25 @@ export default function UserInput(){
         duration: 10
     });
 
+    // to update the data on the ui when user enters the input
+    function handleChange(inputIdentifier, newValue){
+        setUserInput((prevUserInput) => {
+            return {
+                ...prevUserInput,            // this is syntax is used to copy the data stored in the prevUserInput
+                [inputIdentifier]: newValue    // and this JS syntax is used to set the object's property value 
+            }
+        });
+    }
+
     return <form id="user-input">
         <div className="input-group">
             <p>
                 <label>Initial Investment</label>
-                <input type="number" required value={userInput.initialInvestment}/>
+                <input type="number" required onChange={(event) => handleChange('initialInvestment',event.target.value)}/>
             </p>
             <p>
                 <label>Annual Investment</label>
-                <input type="number" required value={userInput.annualInvestment}/>
+                <input type="number" required />
             </p>
         </div>
         <div className="input-group">
